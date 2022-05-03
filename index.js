a59aba9144bb2c0e01bdc7f49050ca87afc08051
azeezade <azeezade04@gmail.com>
2022-05-03 21:48:34 +0100

    Made some significant changes to workflow

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   const PORT = process.env.PORT || 3000;
   const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
 
-  bot = new Telegraf(API_TOKEN);
+  // bot = new Telegraf(API_TOKEN);
 
   bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
   bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
